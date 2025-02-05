9fa5a7515f69d2954786dc2c469d09390b371796
melanie <melanieleon20@hotmail.com>
2025-02-05 10:38:21 -0500

    Se agrego un segundo componente Album y ya se refleja en el html.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContactoComponent } from './shared/contacto/contacto.component';
+import { AlbumComponent } from './shared/album/album.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ContactoComponent],
+  imports: [RouterOutlet, ContactoComponent, AlbumComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
