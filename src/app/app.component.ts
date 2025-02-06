67231b4e5295df1f11218e347b70910e28a644b2
melanie <melanieleon20@hotmail.com>
2025-02-06 10:29:26 -0500

    Se creo interfaz y servicios, para agregar imagenes al card en el html.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { ContactoComponent } from './shared/contacto/contacto.component';
 import { AlbumComponent } from './shared/album/album.component';
 import { CardComponent } from './shared/card/card.component';
+import { RecursosService } from './servicios/recursos.service';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ContactoComponent, AlbumComponent, CardComponent],
+  imports: [RouterOutlet, ContactoComponent, AlbumComponent, CardComponent, HttpClientModule],
+  providers:[RecursosService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Ejemplo1Angular';
-}
+  constructor(private recursosService: RecursosService){}
+  }
+
--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HttpClient } from '@angular/common/http';
+import { Foto } from '../../interfaz/foto';
+import { RecursosService } from '../../servicios/recursos.service';
 @Component({
   selector: 'app-card',
   imports: [CommonModule],
@@ -9,6 +10,12 @@ import { CommonModule } from '@angular/common';
   styleUrl: './card.component.css'
 })
 export class CardComponent {
+  fotos: Foto[] = [];
   cards = Array(9).fill(0);
+  constructor(private recursosService: RecursosService){
+    recursosService.obtenerDatos().subscribe(respuesta=> {
+      this.fotos = respuesta as Array<Foto>
+    })
+  }
 
 }
